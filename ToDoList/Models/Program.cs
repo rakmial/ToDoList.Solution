59b903dfcd042546df88260b087d64e523b599b3
Joshua Lovelace <rakmial@gmail.com>
2020-09-07 10:28:23 -0700

    CLI v1 done, QA test begin

--- ToDoList/Models/Program.cs
+++ ToDoList/Models/Program.cs
@@ -9,11 +9,14 @@ namespace ToDoList.Models
     public static void Main()
     {
       bool exitProgram = false;
+      int listCount = 0;
+      string response;
       string listEmpty = "You currently have nothing on your list.";
       string prompt = "What would you like to do? [add / remove / exit]";
       string descriptionQuery = "Type what you want to put on your list.";
-      string response;
+      string removeQuery = $"Enter number between 1 and {listCount} to remove that item.";
       List<Item> emptyList = new List<Item> {};
+      List<Item> currentList;
       Console.WriteLine("Welcome to this CLI To-Do List.");
       while (!exitProgram)
       {
@@ -23,14 +26,25 @@ namespace ToDoList.Models
         }
         else
         {
-          Console.WriteLine(Item.GetAll());
+          Console.WriteLine(Item.GetPretty());
         }
         Console.WriteLine(prompt);
         response = Console.ReadLine();
         if  (response == "add")
         {
           Console.WriteLine(descriptionQuery);
-
+          Item newItem = new Item(Console.ReadLine());
+        }
+        else if (response == "remove")
+        {
+          currentList = Item.GetAll();
+          listCount = currentList.Count;
+          Console.WriteLine(removeQuery);
+          Item.Remove(int.Parse(Console.ReadLine()));
+        }
+        else if (response == "exit")
+        {
+          exitProgram = true;
         }
       }
     }
